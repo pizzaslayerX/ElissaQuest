4d6fb001686725707612e6887f69b2aacb29ca7d
Jacob <Jacob@Jacob-HP>
2018-08-06 00:12:26 -0500

    Can draw a pic now.

--- Main/src/main/java/ElissaQuest.java
+++ Main/src/main/java/ElissaQuest.java
@@ -6,11 +6,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.LayoutManager;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -20,6 +25,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
@@ -33,8 +39,13 @@ public class ElissaQuest extends Applet implements KeyListener, Runnable, MouseL
 	public static JFrame frame;
 	public static FlowLayout layout;
 	public static Canvas canvas;
-	public static boolean isRunning = true;
+	public static boolean isRunning = false;
+	private Graphics g;
+	private BufferStrategy bs;
+	private Thread t;
 	public Player player = new Player();
+
+    private BufferedImage testImage;
 	public final List<String> returnText = new LinkedList<String>();
 	
 	public void mouseClicked(MouseEvent e) {
@@ -115,11 +126,52 @@ public class ElissaQuest extends Applet implements KeyListener, Runnable, MouseL
 		frame.setSize(SCREEN_SIZE);
 		frame.setTitle(TITLE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		canvas.setPreferredSize(GAME_SIZE);
+        canvas.setMaximumSize(GAME_SIZE);
+        canvas.setMinimumSize(GAME_SIZE);
 		frame.add(canvas);
 		frame.add(main);
 		frame.setVisible(true);
+		frame.pack();
 		main.start();
+
 	}
+	
+	private static final class ImageLoader
+    {
+
+        static BufferedImage loadImage(String fileName)
+        {
+            BufferedImage bi = null;
+            //System.err.println("....setimg...." + fileName);
+
+            try {
+                bi = ImageIO.read(new File(fileName)); 
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Image could not be read");
+                System.exit(1);
+            }
+
+            return bi;
+        }
+    }
+	
+	private void render() {
+        bs = getCanvas().getBufferStrategy();
+
+        if (bs == null) {
+            System.out.println("bs is null....");
+            getCanvas().createBufferStrategy(3);
+            return;
+        }
+
+
+        g = getCanvas().getGraphics();
+        g.drawImage(testImage, 20, 20, null);
+    }
 	
 	public ElissaQuest() {
 		setPreferredSize(SCREEN_SIZE);
@@ -128,19 +180,34 @@ public class ElissaQuest extends Applet implements KeyListener, Runnable, MouseL
 		canvas = new Canvas();
 	}
 	
-	public void start() {
-		init();
-		new Thread(this).start();
-	}
-	
 	public void init() {
 		setFocusable(true);
+		testImage = ImageLoader.loadImage("src/res/pics/test.jpg");
 		//textIn.addKeyListener(this);
 		//text.addMouseListener(this);
 	}
 	
-	public void run() {
+	@Override
+	public void start() {
+		if (isRunning) 
+			 return;
+	       isRunning = true;
 		new Thread(this).start();
+	}
+	
+	@Override
+	public void run() {
+		init();
+        System.out.println("run..." + isRunning);
+        while (isRunning) {
+            //System.err.println("run..." + running);
+            tick();
+            render();
+        }
+	}
+	
+	public void tick() {
+		
 	}
 	
 	public void pause(int i) {
@@ -152,5 +219,16 @@ public class ElissaQuest extends Applet implements KeyListener, Runnable, MouseL
 	public void stop() {
 		isRunning = false;
 	}
+	
+	public Canvas getCanvas() {
+
+        if(canvas == null)
+        {
+            System.out.println("Canvas is null");
+            return null;
+        }
+
+        return canvas;
+    }
 	
 }
