c46f984fe553a6646132c22e3e119f8ce7e5b8b4
Jacob Wechsler <frogkabobs@gmail.com>
2018-08-09 04:41:11 -0500

    h

--- Main/src/main/java/entities/Interactive.java
+++ Main/src/main/java/entities/Interactive.java
@@ -1,8 +1,9 @@
 package entities;
-import run.ElissaRunner;
 import run.GamePlay;
 
 public interface Interactive {
 	public void interact(GamePlay g);
-	public void disappear(Interactive[][] arr, int a, int b);
+	default public void disappear(Interactive[][] arr, int a, int b) {
+		arr[a][b]=null;
+	}
 }
