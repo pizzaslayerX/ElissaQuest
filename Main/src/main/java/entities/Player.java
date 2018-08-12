47f35e418bfd9ce942dba3d52fd49672ca5d12e8
Johnson_891609 <Johnson_891609@MLISD-D0235L.leanderisd.org>
2018-08-11 22:03:28 -0500

    weapontweek

--- Main/src/main/java/entities/Player.java
+++ Main/src/main/java/entities/Player.java
@@ -46,7 +46,7 @@ public class Player extends Entity{
 		inventory.add(Consumable.Consumables.uberPotion());
 		inventory.add(Consumable.Consumables.potion());
 		inventory.add(Consumable.Consumables.potion());
-		inventory.add(Weapon.Weapons.critStaff());
+		inventory.add(Weapon.Weapons.fist());
 		//new StatusEffect("accuracy",1,3).addTo(this);
 		//new StatusEffect("precision",1,3).addTo(this);
 	}
